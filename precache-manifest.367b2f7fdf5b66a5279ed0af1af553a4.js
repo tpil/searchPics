6ed2cf7dcbb189e937af896d6105daa03d4d3d1e
teopyl <teopyl@gmail.com>
2020-08-22 18:15:56 +0300

    Updates

---
+++ precache-manifest.367b2f7fdf5b66a5279ed0af1af553a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc896f7e080279eb96b3f91c49bdd01",
+    "url": "/searchPics/index.html"
+  },
+  {
+    "revision": "23d780b77c6794c26f89",
+    "url": "/searchPics/static/css/main.f3aa1471.chunk.css"
+  },
+  {
+    "revision": "3f9c29946aa5a0e38875",
+    "url": "/searchPics/static/js/2.66b9be77.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/searchPics/static/js/2.66b9be77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d780b77c6794c26f89",
+    "url": "/searchPics/static/js/main.26f08e41.chunk.js"
+  },
+  {
+    "revision": "4bd867c227e0e6175841",
+    "url": "/searchPics/static/js/runtime-main.bea98650.js"
+  }
+]);
